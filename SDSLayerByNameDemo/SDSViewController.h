f99ee444fc48127e0a06dcb511ac7b59b5e350c3
Sergio De Simone <sdesimone@gmx.net>
2013-09-11 19:53:43 +0200

    initial commit

--- SDSLayerByNameDemo/SDSViewController.h
+++ SDSLayerByNameDemo/SDSViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SDSViewController : UIViewController
+@interface SDSViewController : UIViewController <UITextFieldDelegate>
 
 @end
--- SDSLayerByNameDemo/SDSViewController.m
+++ SDSLayerByNameDemo/SDSViewController.m
@@ -7,23 +7,72 @@
 //
 
 #import "SDSViewController.h"
+#import "CALayer+SDSLayerByName.h"
 
-@interface SDSViewController ()
+@interface SDSViewController () 
 
 @end
 
 @implementation SDSViewController
 
+/////////////////////////////////////////////////////////////////////////////////////////
+- (void)addImageNamed:(NSString*)imgName {
+    
+    UIImage* img = [UIImage imageNamed:imgName];
+    CALayer* imgLayer = [CALayer layer];
+    imgLayer.bounds = (CGRect){CGPointZero, img.size};
+    
+    imgLayer.contents = (id)img.CGImage;
+    imgLayer.position = self.view.center;
+    imgLayer.name = imgName;
+    
+    [self.view.layer addSublayer:imgLayer];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    [self addImageNamed:@"img1.jpg"];
+    [self addImageNamed:@"img2.jpg"];
+    [self addImageNamed:@"img3.jpg"];
+    [self hideLayers];
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////
+- (void)hideLayers {
+
+    [self.view.layer layerByName:@"img1.jpg"].opacity = 0.0;
+    [self.view.layer layerByName:@"img2.jpg"].opacity = 0.0;
+    [self.view.layer layerByName:@"img3.jpg"].opacity = 0.0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////
+- (void)textFieldDidEndEditing:(UITextField*)textField {
+    
+    NSString* name = textField.text;
+    
+    [self hideLayers];
+    
+    if ([self.view.layer layerByName:name])
+        [self.view.layer layerByName:name].opacity = 1.0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+  
+    [textField resignFirstResponder];
+//    [self textFieldDidEndEditing:textField];
+    return YES;
+}
+
 
 @end
