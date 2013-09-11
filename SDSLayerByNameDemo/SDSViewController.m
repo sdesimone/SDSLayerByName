390c84a3a18565077f0d32023f219771b9bf926d
Sergio De Simone <sdesimone@gmx.net>
2013-09-11 19:11:48 +0200

    Initial Commit

---
+++ SDSLayerByNameDemo/SDSViewController.m
@@ -0,0 +1,29 @@
+//
+//  SDSViewController.m
+//  SDSLayerByNameDemo
+//
+//  Created by sergio on 9/11/13.
+//  Copyright (c) 2013 Sergio De Simone, Freescapes Labs. All rights reserved.
+//
+
+#import "SDSViewController.h"
+
+@interface SDSViewController ()
+
+@end
+
+@implementation SDSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
